c5d11607f28c9b198478adec84e894e70b18e619
Kieran Hillman <developedbykieran@gmail.com>
2023-11-23 11:03:21 +0000

    Wire up index view url for home page

--- musicality/urls.py
+++ musicality/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from events.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
