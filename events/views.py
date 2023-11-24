af341f75eba99f4ecbf4d00d8a2420a6e325d76c
Kieran Hillman <developedbykieran@gmail.com>
2023-11-24 10:24:04 +0000

    Refactor views and create event detail view for individual event

--- events/views.py
+++ events/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import EventModel
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('TESTING')
+    events = EventModel.objects.filter(approved=True)
+
+    context = {
+        'events': events,
+    }
+    return render(request, 'events/index.html', context)
+
+def event_detail(request, event_id):
+    event = get_object_or_404(EventModel, pk=event_id)
+
+    context = {
+        'event': event,
+    }
+
+    return render(request, 'events/event_detail.html', context)
