2e5edb766b98678c0b79e62163892b4feaf6d685
Kieran Hillman <developedbykieran@gmail.com>
2023-11-24 14:02:01 +0000

    Adjust unique properties so that event date, time and loctation are unique together, then migrate

--- events/models.py
+++ events/models.py
@@ -16,14 +16,15 @@ class EventModel(models.Model):
     # Event image here when cloudinary is set up
     description = models.TextField()
     ticket_price = models.FloatField(default=0)
-    event_date = models.DateField(unique=True)
-    event_time = models.TimeField(unique=True)
+    event_date = models.DateField()
+    event_time = models.TimeField()
     city = models.CharField(max_length=50)
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
+        unique_together = [['event_date', 'event_time', 'city']]
 
     def __str__(self):
         return f"Event name: {self.title} held in {self.city} on {self.event_date}"
