a0e71115c46ca1f2f2f3d2b9b33be5c025900868
Kieran Hillman <developedbykieran@gmail.com>
2023-11-24 10:25:20 +0000

    Add django extensions as installed app, add templates directory

--- musicality/settings.py
+++ musicality/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'musicality.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
