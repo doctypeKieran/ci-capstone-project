020fe13299eb004df629a01df9e903255ac70499
Kieran Hillman <developedbykieran@gmail.com>
2023-11-24 10:23:30 +0000

    Create index and individual event urls

---
+++ events/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'events'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+]
