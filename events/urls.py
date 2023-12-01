1a79aacfc183abad014bcd3324af6afba2810837
Kieran Hillman <developedbykieran@gmail.com>
2023-12-01 13:44:33 +0000

    Add new path for create event template and view

--- events/urls.py
+++ events/urls.py
@@ -7,4 +7,5 @@ app_name = 'events'
 urlpatterns = [
     path('', views.index, name='index'),
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('create-event/', views.create_event, name='create_event'),
 ]
