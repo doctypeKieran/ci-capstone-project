2ac01bcfc072404667f786291b47c6c86903bb63
Kieran Hillman <developedbykieran@gmail.com>
2023-12-01 09:32:26 +0000

    Add new venue field, change unique_together for venue, make migrations

--- events/models.py
+++ events/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
+
+
 class EventModel(models.Model):
     """
     Event model with fields for title, teaser, creator, image, description,
@@ -19,12 +21,16 @@ class EventModel(models.Model):
     event_date = models.DateField()
     event_time = models.TimeField()
     city = models.CharField(max_length=50)
+    venue = models.CharField(max_length=100, default="Royal Albert Hall", null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
+
     class Meta:
         ordering = ['event_date']
-        unique_together = [['event_date', 'event_time', 'city']]
+        # The code below ensures that events can't take place at the same date and time at the same city.
+        unique_together = [['event_date', 'event_time', 'venue']]
+
 
     def __str__(self):
         return f"Event name: {self.title} held in {self.city} on {self.event_date}"
