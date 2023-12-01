6b4f520fdecb59b2d6dffbc03ed672cf5020e17a
Kieran Hillman <developedbykieran@gmail.com>
2023-12-01 13:45:14 +0000

    Define new create event view using the event creation form for create event template

--- events/views.py
+++ events/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from .models import EventModel
+from .forms import EventCreationForm
 
 # Create your views here.
+
 
 def index(request):
     events = EventModel.objects.filter(approved=True)
@@ -13,6 +16,7 @@ def index(request):
     }
     return render(request, 'events/index.html', context)
 
+
 def event_detail(request, event_id):
     event = get_object_or_404(EventModel, pk=event_id)
 
@@ -21,3 +25,22 @@ def event_detail(request, event_id):
     }
 
     return render(request, 'events/event_detail.html', context)
+
+
+@login_required
+def create_event(request):
+    if request.method == 'POST':
+        form = EventCreationForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.creator = request.user
+            event.save()
+            return redirect('index')
+    else:
+        form = EventCreationForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'events/create_event.html', context)
