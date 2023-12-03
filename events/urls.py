b868da68cc83d13b72f4e9f4acfbb70bf6b22eda
Kieran Hillman <developedbykieran@gmail.com>
2023-12-03 11:26:00 +0000

    Create new url path for user pending events

--- events/urls.py
+++ events/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create-event/', views.create_event, name='create_event'),
     path('approval/list/', views.event_approval_list, name='event_approval_list'),
     path('approval/approve/<int:event_id>/', views.event_approval, name='event_approval'),
+    path('user-pending-events', views.user_pending_events, name='user_pending_events'),
 ]
