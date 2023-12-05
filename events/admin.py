1e98b4990fc6810a9695d88b0aa8e78747fa819d
Kieran Hillman <developedbykieran@gmail.com>
2023-12-05 13:47:43 +0000

    Register book event and review models for admin site

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import EventModel
+from .models import EventModel, BookEventModel, Review
 
 # Register your models here.
 
 
 admin.site.register(EventModel)
+admin.site.register(BookEventModel)
+admin.site.register(Review)
