2451f1f135736ea36ddac416266efa80ac52c9e7
Kieran Hillman <developedbykieran@gmail.com>
2023-12-07 16:11:32 +0000

    Change single quotes to double quotes to pass JS linter

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
-const deleteButtons = document.getElementsByClassName('delete-event-btn');
-const confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
+const deleteButtons = document.getElementsByClassName("delete-event-btn");
+const confirmDeleteBtn = document.getElementById("confirmDeleteBtn");
 
 for (let button of deleteButtons) {
-    button.addEventListener('click', (e) => {
-        let deleteEventId = e.target.getAttribute('data-event-id');
+    button.addEventListener("click", (e) => {
+        let deleteEventId = e.target.getAttribute("data-event-id");
         confirmDeleteBtn.href = `events/delete-event/${deleteEventId}`;
     })
 }
 
-const navMenuIcon = document.getElementById('nav-menu-icon');
+const navMenuIcon = document.getElementById("nav-menu-icon");
 
-navMenuIcon.addEventListener('click', () => {
+navMenuIcon.addEventListener("click", () => {
     navMenuIcon.innerHTML = navMenuIcon.innerHTML === `<i class="hamburger-icon fa-solid fa-bars" aria-hidden="true"></i>` ? `<i class="hamburger-icon fa-solid fa-xmark"></i>` : `<i class="hamburger-icon fa-solid fa-bars" aria-hidden="true"></i>`;
 })
